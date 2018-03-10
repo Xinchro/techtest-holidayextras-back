ec52cdeb3275a8c6a025c1ff8285413f500e78dc
Xinchro <TheXrocks@gmail.com>
2018-03-10 12:01:41 +0000

    added express boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world!")
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => res.send("Hello world!"))
+
+const port = 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
