60e585a80f0bd280362522cf3c9e480f37ae2e25
Xinchro <TheXrocks@gmail.com>
2018-03-11 12:34:17 +0000

    added post test for /user/new endpoint and enabled mocha console colors

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ const chai = require("chai")
 const chaiHttp = require("chai-http")
 
 const should = chai.should()
+const expect = chai.expect
 
 const appUrl = process.env.APP_URL
 
@@ -17,6 +18,34 @@ describe("/user", () => {
           res.should.have.status(200)
           res.body.should.be.a("object")
           res.body.error.should.equal("Did you mean to POST this url?")
+          done()
+        })
+    })
+  })
+
+  describe("POST /user/new", () => {
+    it("it should POST a new user and receive an object with the same data and a new id", (done) => {
+      chai.request(appUrl)
+        .post("/user/new")
+        .send(
+          {
+            "email": "test@test.test",
+            "forename": "Namey",
+            "surname": "McNameFace",
+            "created": (new Date()).toISOString()
+          }
+        )
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a("object")
+          res.body.should.have.keys(
+            "__v",
+            "_id",
+            "email",
+            "forename",
+            "surname",
+            "created"
+          )
           done()
         })
     })
